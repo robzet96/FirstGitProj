558f5853d27b16463e63c820199d9543332c2d9d
dasad44 <dasad33_79@o2.pl>
2022-03-03 22:16:16 +0100

    rrerwre

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -1,31 +1,28 @@
 ﻿int correctPin = 1111;
 
 Console.WriteLine("Ppin");
-Console.WriteLine("Po pin");
-Console.WriteLine("Podin");
+dasdasd
+    dsdadasdsadasd
 Console.WriteLine("Poin");
 Console.WriteLine("Popin");
 Console.WriteLine("Po pin");
-Console.WriteLine("Ppin");
-Console.WriteLine("Pj pin");
+dasdasdsaasasas
 Console.WriteLine("Ppin");
 Console.WriteLine("Po pin");
 
 for (int i = 2; i >= 0; i--)
 {
-    Console.WriteLine("Podaj pin");
-    int pin = Convert.ToInt32(Console.ReadLine());
-
+dsasdasdasdasdasd
+        dsasdasdasdasdasdasdasd
+dasdasdasf asfasfasdf asdf adfad
     if(pin == correctPin)
     {
         Console.WriteLine("Zalogowano");
         break;
     }
-    else if(i == 0)
-    {
-        Console.WriteLine("Zly pin, zablokowano dostep do konta");
-    }
-    else
+dasasdasdasdasd
+    dsdasdasdasdasdas
+dasasdasdasdasd
     {
         Console.WriteLine("Zly pin, pozostalo " + i + " prob");
     }
