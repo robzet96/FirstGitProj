8d44cbc84ce4a14aae4df3a57d5e67e5daf9cac2
dasad44 <dasad33_79@o2.pl>
2022-03-03 22:04:27 +0100

    dsdas

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,32 @@
+﻿int correctPin = 1111;
+
+Console.WriteLine("Podaj pin");
+Console.WriteLine("Podaj pin");
+Console.WriteLine("Podaj pin");
+Console.WriteLine("Podaj pin");
+Console.WriteLine("Podaj pin");
+Console.WriteLine("Podaj pin");
+Console.WriteLine("Podaj pin");
+Console.WriteLine("Podaj pin");
+Console.WriteLine("Podaj pin");
+Console.WriteLine("Podaj pin");
+
+for (int i = 2; i >= 0; i--)
+{
+    Console.WriteLine("Podaj pin");
+    int pin = Convert.ToInt32(Console.ReadLine());
+
+    if(pin == correctPin)
+    {
+        Console.WriteLine("Zalogowano");
+        break;
+    }
+    else if(i == 0)
+    {
+        Console.WriteLine("Zly pin, zablokowano dostep do konta");
+    }
+    else
+    {
+        Console.WriteLine("Zly pin, pozostalo " + i + " prob");
+    }
+}
